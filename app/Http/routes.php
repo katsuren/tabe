90ab2d2c0ec173dfb199f9b41b694369de99f770
katsuren <katsuren.com@gmail.com>
2015-05-06 13:29:15 +0900

    ユーザー登録のルーティング/画面作成

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,6 +13,8 @@
 
 Route::get('/', 'WelcomeController@index');
 
+Route::get('/user/signup', 'UserController@signup');
+
 Route::get('home', 'HomeController@index');
 
 Route::controllers([
---
+++ resources/views/user/signup.blade.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>ユーザー登録</title>
+</head>
+<body>
+	<form method="POST">
+		ユーザー名を入力してください<br>
+		<input name="name" style="width: 200px;"></input><br>
+		<button>登録</button>
+	</form>
+</body>
+</html>
